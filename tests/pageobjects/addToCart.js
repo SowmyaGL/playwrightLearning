33d56a67a52f326d6384be96ee157b45072a48d4
ArunkumarG66 <arunkumarg9816@gmail.com>
2026-02-13 12:25:23 +0530

    changes

--- tests/pageobjects/addToCart.js
+++ tests/pageobjects/addToCart.js
@@ -1,4 +1,3 @@
-
 export async function addToCart(page, productName) {
   await page
     .locator('.product', {
@@ -6,16 +5,4 @@ export async function addToCart(page, productName) {
     })
     .locator('.product-action button')
     .click();
-}
-
-export async function Abcd(page, productName) {
-  console.log('abcd fn')
-}
-
-export async function Abcd2(page, productName) {
-  console.log('abcd fn 2')
-}
-
-export async function Abcd3(page, productName) {
-  console.log('abcd fn3')
 }
--- tests/rahulshettyacademy.spec.js
+++ tests/rahulshettyacademy.spec.js
@@ -1,13 +1,45 @@
-const { test } = require('@playwright/test');
-import { addToCart, Abcd, Abcd2, Abcd3 } from './pageobjects/addToCart';
+const { test, expect } = require('@playwright/test');
 
-
-test('add  items to cart ', async({page}) =>{
+test('add  items to cart ', async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/");
-    const addToCartItems =["Brocolli - 1 Kg", "Cauliflower - 1 Kg", "Cucumber - 1 Kg"];
+    const addToCartItems = ["Brocolli - 1 Kg", "Cauliflower - 1 Kg", "Cucumber - 1 Kg"];
 
-        for (const item of addToCartItems){
-        await addToCart(page, item);
-    }    
+    let expectedTotal = 0;
+
+    for (const item of addToCartItems) {
+
+        const product = page.locator('.product', {
+            has: page.locator('.product-name', { hasText: item })
+        });
+
+        const priceText = await product.locator('.product-price').textContent();
+
+        // convert string to number
+        const price = parseInt(priceText.trim());
+
+        expectedTotal += price;   // ✅ add to total
+
+        await product.locator('.product-action button').click();
+    }
+
+    const cartTotalLocator = page.locator(
+        '.cart-info > table > tbody > tr:nth-child(2) > td:nth-child(3)'
+    );
+
+    await expect(cartTotalLocator).toHaveText(expectedTotal.toString());
+
+    console.log("Total price:", expectedTotal);
+
+    // login
+    // items list
+    // all selected items add to card
+    // selected items price = add to cart price
+    // no of selected items = add to cart selected items
+
+    // address 
+    // pay
+    // verify price as same as in the add to cart
+    // UPI 
+    // verifying success message
 })
